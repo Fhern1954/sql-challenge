90e1f72579b4f8de1d913f369c5d725783c8687f
Fhern1954 <63937814+Fhern1955@users.noreply.github.com>
2021-02-09 21:51:35 -0500

    organizing files into folders

---
+++ EmployeeSQL/code/schema.sql
@@ -0,0 +1,70 @@
+﻿--Drop table if one exists.
+Drop Table departments;
+
+--Create Table departments.
+CREATE TABLE departments (
+dept_no VARCHAR (10) PRIMARY KEY,
+dept_name VARCHAR(30)
+);
+
+--Drop table if one exists.
+DROP TABLE titles;
+
+--Create Table titles.
+CREATE TABLE titles (
+title_id VARCHAR (10) PRIMARY KEY,
+title TEXT
+);
+
+
+--Drop table if one exists.
+DROP TABLE employees;
+
+--Create Table employees.
+CREATE TABLE employees(
+emp_no INT PRIMARY KEY,
+emp_title_id VARCHAR(10),
+birth_date DATE,
+first_name VARCHAR (20),
+last_name VARCHAR (20),
+sex VARCHAR (5),
+hire_date DATE,
+FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)	
+);
+
+
+--Drop Table if one exists.
+DROP TABLE salaries;
+
+--Create Table salaries.
+CREATE TABLE salaries (
+emp_no INT,
+salary INT,
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no)	
+);
+
+
+--Drop Table if one exists.
+DROP TABLE dept_emp;
+
+--Create Table dept_emp.
+CREATE TABLE dept_emp (
+	emp_no INT,
+	dept_no VARCHAR(10),
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+
+--Drop Table if one excists.
+DROP TABLE dept_manager;
+
+--Create Table dept_manager
+CREATE TABLE dept_manager (
+	dept_no VARCHAR (10),
+	emp_no INT,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)	
+);
